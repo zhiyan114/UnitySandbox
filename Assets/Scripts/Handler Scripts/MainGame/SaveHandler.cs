690f3a2131b761773c967ec547f7328474610514
zhiyan114 <20156291+zhiyan114@users.noreply.github.com>
2022-01-04 11:54:09 -0500

    Tweaked MainGame UI a bit

--- Assets/Scripts/Handler Scripts/MainGame/SaveHandler.cs
+++ Assets/Scripts/Handler Scripts/MainGame/SaveHandler.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SaveHandler : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject SaveMsg;
+    private Button SaveBtn;
     void Start()
     {
+        SaveBtn = gameObject.GetComponent<Button>();
         InvokeRepeating("DoSave", 30, 15);
     }
     IEnumerator ShowSaveMsg()
@@ -15,9 +18,12 @@ public class SaveHandler : MonoBehaviour
         SaveMsg.SetActive(true);
         yield return new WaitForSeconds(3);
         SaveMsg.SetActive(false);
+        SaveBtn.interactable = true;
     }
     public void DoSave()
     {
+        if (!SaveBtn.interactable) return;
+        SaveBtn.interactable = false;
         StartCoroutine(ShowSaveMsg());
         SaveManager.SaveToDisk();
     }
